1da96f1b6834106d52152dc97b812a7a771c9a5c
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-07-13 15:08:01 +0530

    Data Collection done(>5000 events) for all csv

--- evenful.py
+++ evenful.py
@@ -7,8 +7,8 @@ count=0
 img=1
 
 with open(file_name,'rb') as csvfile:
-	with open('datafile.csv','a+') as csvfile1:
-		fieldnames = ['title', 'venue_name', 'key']
+	with open('data1.csv','a+') as csvfile1:
+		fieldnames = ['id', 'title', 'venue_name', 'key']
 		datawriter = csv.DictWriter(csvfile1, fieldnames=fieldnames)
 		data = csv.reader(csvfile)
 		for row in data:
@@ -24,7 +24,7 @@ with open(file_name,'rb') as csvfile:
 						print "%s at %s of %s" % (event['title'], event['venue_name'],key)
 						count=count+1
 						print count
-    						datawriter.writerow({'title': event['title'], 'venue_name': event['venue_name'], 'key': key})
+    						datawriter.writerow({'id': event['id'], 'title': event['title'], 'venue_name': event['venue_name'], 'key': key})
 			except:
 				print 'excpetion'
 
