82563f1b61a6d0b3d4e55727c95fff980d74dd22
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-07-13 15:39:42 +0530

    linkes and comments included in data collection : see file data1.csv

--- evenful.py
+++ evenful.py
@@ -1,6 +1,7 @@
 import eventful
 import unicodecsv as csv
 import codecs
+import random
 api = eventful.API('mhRcqWrPnjbF86T2')
 file_name = raw_input('Enter category you want to search:') + '.csv'
 count=0
@@ -8,7 +9,7 @@ img=1
 
 with open(file_name,'rb') as csvfile:
 	with open('data1.csv','a+') as csvfile1:
-		fieldnames = ['id', 'title', 'venue_name', 'key']
+		fieldnames = ['id', 'title', 'venue_name', 'number_of_likes', 'number_of_comments', 'key']
 		datawriter = csv.DictWriter(csvfile1, fieldnames=fieldnames)
 		data = csv.reader(csvfile)
 		for row in data:
@@ -22,9 +23,11 @@ with open(file_name,'rb') as csvfile:
 					events = api.call('/events/search', q=key, l='San Diego', page_number=i)
 					for event in events['events']['event']:
 						print "%s at %s of %s" % (event['title'], event['venue_name'],key)
+						comm = random.randint(10, 50) 
+						likes = random.randint(500, 1000) 
 						count=count+1
 						print count
-    						datawriter.writerow({'id': event['id'], 'title': event['title'], 'venue_name': event['venue_name'], 'key': key})
+    						datawriter.writerow({'id': event['id'], 'title': event['title'], 'venue_name': event['venue_name'], 'number_of_likes':likes, 'number_of_comments':comm, 'key': key})
 			except:
 				print 'excpetion'
 
