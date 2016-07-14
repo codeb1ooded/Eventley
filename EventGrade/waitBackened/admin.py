c1a116323c4b0ee9bee16700df5394769e67e06e
saloni1996 <salonibhartia1996@gmail.com>
2016-07-14 13:32:37 +0530

    little changes

--- EventGrade/waitBackened/admin.py
+++ EventGrade/waitBackened/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import user,event
+from .models import event
 # Register your models here.
 admin.site.register(user)
-admin.site.register(event)
+#admin.site.register(event)
