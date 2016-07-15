e208c4eca5fea6825206ad7abf49fb3c182da0c4
saloni1996 <salonibhartia1996@gmail.com>
2016-07-15 13:10:51 +0530

    Some changes done

--- EventGrade/waitBackened/collect_events.py
+++ EventGrade/waitBackened/collect_events.py
@@ -9,6 +9,7 @@ img=1
 def event_collection(latitude, longtitude, covered_area, units_distance, select_category_event):
 	file_name = "csv/"+select_category_event + '.csv'
 	count=0
+	links=0 
 	with open(file_name,'rb') as csvfile:
 		with open('csv/datafile.csv','a+') as csvfile1:
 			fieldnames = ['id', 'title', 'venue_name', 'number_of_likes', 'number_of_comments', 'key']
@@ -17,7 +18,7 @@ def event_collection(latitude, longtitude, covered_area, units_distance, select_
 			for row in data:
 				key=''.join(row)
 				i=0
-       
+       			
 				events = api.call('/events/search', q=key, l=latitude+", "+longtitude, within=covered_area+"", units=units_distance+"")
 				num_of_pages = int(events['page_count'])
 				try: 
@@ -26,15 +27,16 @@ def event_collection(latitude, longtitude, covered_area, units_distance, select_
 						for Event in events['events']['event']:
 							print "%s at %s of %s" % (Event['title'], Event['venue_name'],key)
 							comm = random.randint(10, 50) 
-							likes = random.randint(500, 1000) 
+							likes = random.randint(500, 1000)
 							eventobj = event()
-							eventobj.event_id = Event['id']
-							eventobj.event_name = Event['title']
-							eventobj.venue_name = Event['venue_name']
-							eventobj.no_likes = likes
-							eventobj.category = key
-							eventobj.no_comments = comm
-							event.save(eventobj)
+							eventobj['Eventid'] = Event['id']
+							eventobj['Eventname'] = Event['title']
+							eventobj['Venuename'] = Event['venue_name']
+							eventobj['Nolikes'] = likes
+							eventobj['Category'] = key
+							eventobj['Nocomments'] = comm
+							eventobj['Nolinks'] = links
+							eventobj.save()
 							count=count+1
 							print count
     							datawriter.writerow({'id': Event['id'], 'title': Event['title'], 'venue_name': Event['venue_name'], 'number_of_likes':likes, 'number_of_comments':comm, 'key': key})
