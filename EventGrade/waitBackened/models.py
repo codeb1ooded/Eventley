ae8aeb2dbe9fd4fa0e8e61b0fea166ec63db99c0
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-07-16 14:06:42 +0530

    Database modified, analysis done and score saved in database

--- EventGrade/waitBackened/models.py
+++ EventGrade/waitBackened/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 # Create your models here.
 class event(models.Model):
-	Eventid=models.CharField(max_length=100)
-	Eventname=models.CharField(max_length=100)
-	Venuename=models.CharField(max_length=100)
-	Nolikes=models.IntegerField()
-	Category=models.CharField(max_length=100)
-	Nocomments=models.IntegerField()
-	Nolinks=models.IntegerField()
-	#score=no_links+no_comments+no_likes
+	Eventid = models.CharField(max_length=100)
+	Eventname = models.CharField(max_length=100)
+	Venuename = models.CharField(max_length=100)
+	Nolikes = models.IntegerField()
+	Category = models.CharField(max_length=100)
+	Nocomments = models.IntegerField()
+	Nolinks = models.IntegerField()
+	NoImages = models.IntegerField()
+	Score = models.DecimalField(max_digits=100, decimal_places=5)
+	Date = models.DateTimeField(auto_now=False, auto_now_add=False)
 
 	def _str_(self):
 		return self.event_id + '-' +self.event_name
