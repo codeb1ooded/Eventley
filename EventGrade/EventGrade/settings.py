a7ad871ba1aa27fa9d3278edfcd160881ec79a5f
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-07-18 16:57:50 +0530

    All plots put in single html file and extra files removed

--- EventGrade/EventGrade/settings.py
+++ EventGrade/EventGrade/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'EventGrade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, '')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- EventGrade/visuals/views.py
+++ EventGrade/visuals/views.py
@@ -62,13 +62,15 @@ from django.shortcuts import render
     """
 
 def plot_p(request):
-	plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], filename='my-graph.html', auto_open=False, output_type='file')
-	plot([Bar(x=[1, 2, 3], y=[3, 1, 6])], filename='my-bar.html', auto_open=False, output_type='file')
-	fig = {
+    scatter_diag = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], filename='my-graph.html', auto_open=False, output_type='div')
+    bar_diag = plot([Bar(x=['1', '2', '3'], y=[13, 10, 50])], filename='my-bar.html', auto_open=False, output_type='div')
+    fig = {
 		'data': [{'labels': ['Residential', 'Non-Residential', 'Utility'],
 		'values': [19, 26, 55],
 		'type': 'pie'}],
-		'layout': {'title': 'Forcasted 2014 U.S. PV Installations by Market Segment'}
+		'layout': {'title': 'Event Comparison'}
 		}
-	plot(fig, filename='my-pie.html', auto_open=False, output_type='file')
-	return render(request, 'my-pie.html', Context())
+    pie_chart = plot(fig, filename='my-pie.html', auto_open=False, output_type='div')
+    utf8 = [scatter_diag, u'<br>', bar_diag, u'<br>', pie_chart]
+    html = ''.join(utf8) #str(scatter_diag) + u'<br>' + str(bar_diag) + u'<br>' + str(pie_chart)
+    return HttpResponse(html)
