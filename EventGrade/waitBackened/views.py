9f79e0e80de883fcf0a2b5e86bf50a8384e5844e
Megha Aggarwal <meghaaggarwal493@gmail.com>
2016-07-19 14:06:34 +0530

    Visualizations improved

--- EventGrade/waitBackened/views.py
+++ EventGrade/waitBackened/views.py
@@ -36,10 +36,30 @@ def wait_for_backend(request):
 		scatter_diag = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], filename='my-graph.html', auto_open=False, output_type='div')
     	bar_diag = plot([Bar(x=['1', '2', '3'], y=[13, 10, 50])], filename='my-bar.html', auto_open=False, output_type='div')
     	fig = {
-		'data': [{'labels': ['Residential', 'Non-Residential', 'Utility'],
-		'values': [19, 26, 55],
-		'type': 'pie'}],
-		'layout': {'title': 'Event Comparison'}
+			'data': [{'labels': ['Residential', 'Non-Residential', 'Utility'],
+			'values': [19, 26, 55],
+			'type': 'pie',
+	        'name': 'Event Comparison',
+		    'text': 'likes',
+	    	'textposition': 'inside',
+	        'hoverinfo': 'label+percent+name',
+		    'hole': .4,}],
+			'layout': {'title': 'Event Comparison',
+			'annotations': [{'showarrow': 'False', 'text': 'likes'}]}
+			}
+		pie_chart = plot(fig, filename='my-pie.html', auto_open=False, output_type='div')
+		fig = {
+			'data': [{'x':[1, 2, 3, 4],
+			'y': [10, 11, 12, 13],
+			'mode': 'markers',
+			'marker': { 'color': ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+			'opacity': [1, 0.8, 0.6, 0.4],
+			'size': [40, 60, 80, 100]
+			 }
+			}],
+			'layout': {'title': 'Marker Size and Color'}
 		}
-	return HttpResponseRedirect("/visual/")
-
+		bubble_diag = plot(fig, filename='my-bubble.html', auto_open=False, output_type='div')
+		utf8 = [scatter_diag, u'<br>', bar_diag, u'<br>', pie_chart, u'<br>', bubble_diag]
+		html = ''.join(utf8) #str(scatter_diag) + u'<br>' + str(bar_diag) + u'<br>' + str(pie_chart)
+		return HttpResponse(html)
